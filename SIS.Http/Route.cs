cbabb5ceb221fff3f835a80b82ce600ce55b7fb4
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2020-02-17 13:59:01 +0200

    DemoAppDeleted

---
+++ SIS.Http/Route.cs
@@ -0,0 +1,22 @@
+﻿using SIS.Http;
+using System;
+using System.Net.Http;
+
+namespace SIS.Http
+{
+    public class Route
+    {
+        public Route(string path, HttpMethodType methodType, Func<HttpRequest, HttpResponse> action)
+        {
+           this.Path = path;
+           this.HttpMethod = methodType;
+           this.Action = action;
+        }
+
+        public string Path { get; set; }
+
+        public HttpMethodType HttpMethod { get; set; }
+
+        public Func<HttpRequest,HttpResponse> Action { get; set; }
+    }
+}
